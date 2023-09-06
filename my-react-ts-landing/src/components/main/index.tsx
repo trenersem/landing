0e3a285282af4efc4d07b7230cd714a7bfc9ba03
Serhii Seriogin <serhii.serohin@tacanslabs.com>
2023-09-06 10:39:59 -0400

    add build

--- my-react-ts-landing/src/components/main/index.tsx
+++ my-react-ts-landing/src/components/main/index.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import s from './index.module.scss';
-import { Button, EButtonType, ESvgIconType, SvgIcon, Typography } from '../atoms';
-import { info_1 } from '../../assets';
+import { Button, EButtonType, Typography } from '../atoms';
 
 const Main = () => {
   return (
